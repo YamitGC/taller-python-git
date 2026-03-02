1ba7bf6d3a1b226db052f9a9db90fa05683d51e8
yamitGC <yamitgc08@gmail.com>
2026-03-01 21:28:41 -0500

    Añadida función para calcular el perímetro

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ import math
 def calcular_area(radio):
     return math.pi * radio**2
 
+def calcular_perimetro(radio):
+    return 2 * math.pi * radio
+
+# ... (actualiza el print para mostrar también el perímetro)
+
 if __name__ == "__main__":
     r = float(input("Introduce el radio del círculo: "))
     area = calcular_area(r)
+    perimetro = calcular_perimetro(r)
+    print(f"El perimetro del círculo con radio {r} es: {perimetro} ")
     print(f"El área del círculo con radio {r} es: {area:.2f}")
